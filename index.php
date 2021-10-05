d0312454cd5daa8e8915c6128de36fb04955d114
Muhd Azizi <mr.krab72@gmail.com>
2021-10-05 22:20:50 +0800

    First Version

---
+++ index.php
@@ -0,0 +1,77 @@
+<?php
+$page_title = 'Home Page';
+include ('includes/iheader.html');
+?>
+
+<h1>Kingdom Hearts 3 RE:Mind - Yozora Speedrun Competition!</h1>
+
+	<h2><p>What's "Kingdom Hearts 3 RE:Mind"?</p></h2>
+	<h3><p>Kingdom Hearts 3 is a open world RPG game where the main protagonist, Sora, together with his friends, need gather 7 Guardians of Light and awaken the Power of Waking in order to defeat the real Organization XIII to save the world from endless darkness.<br><br>Kingdom Hearts 3 is the 9th game from the Kingdom Hearts series as this game will summerized and conclude everything that has happened from the 1st game until the 8th game. RE:Mind is the required DLC in order to
+	fight Yozora.</p></h3>
+
+	<h2><p>What's this competition about?</p></h2>
+	<h3><p>We know how tough it is to battle Yozora in Kingdom Hearts 3 RE:Mind. One of the hardest boss battle in the series. Hence, we'll be having a speedrun competition to see who can be the fastest to beat Yozora! Join us and compete to see who is worthy to have the "Keyblade Master" title!</p></h3>
+	
+	<h2><p>What's the criteria to join?</p></h2>
+	<h3><p>- Must have the "Kingdom Hearts 3" game and its DLC (RE:Mind) to participate.</p></h3>
+	<h3><p>- Have a platform to play:- Playstation, Xbox, PC.</p></h3>
+	<h3><p>- Willing to give their best!</p></h3>
+
+
+	<h2><p>What's the prize for winning?</p></h2>
+	<h3><p><span class="first">FIRST PLACE</span> 
+			<span class="arrow" style="margin-left: 20px;">&#8680;</span> Cash Prize and ONE custom made Keyblade 1:1 ratio with your own name engraved to it!</p></h3>
+	<h3><p><span class="second">SECOND PLACE</span>
+			<span class="arrow">&#8680;</span> Cash Prize and ONE Kingdom Key 1:1 ratio!</p></h3>
+	<h3><p><span class="third">THIRD PLACE</span>
+		<span class="arrow" style="margin-left: 19px;">&#8680;</span> Cash Prize!</p></h3>
+
+	<h2><p>THE BATTLE IS ON!</p></h2>
+	<h3><p>Join now and stand a chance to be a Keyblade Master!</p></h3>
+
+
+	<br><br><br>
+
+	<span class="gif"><img src="includes/yozora.gif" style="width: 35%; height: auto; float: right;"></span>
+	<span class="caption">"<i>Time to end this.</i>" - Yozora</span>
+
+	<h2 style="margin-left: 80px; top: -10px; font-size: 2em;"><u>One of the fastest speedrun! (By Luke mSL)</u></h2>
+	<br>
+	<div style="margin-left: 50px;">
+
+		<iframe style="border-radius: 25px;" class="glow" width="600" height="345" src="https://www.youtube-nocookie.com/embed/OY9KQZD9-Fo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</div>
+	
+	<br><br>
+	<h4>
+		**This competition will be held virtually. Any updates for the events will be notified through this website and participant's email address.**
+	</h4>
+
+	<button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
+
+<?php
+include ('includes/ifooter.html');
+?>
+
+
+<script type="text/javascript">
+	//Get the button:
+	mybutton = document.getElementById("myBtn");
+
+	// When the user scrolls down 20px from the top of the document, show the button
+	window.onscroll = function() {scrollFunction()};
+
+	function scrollFunction() {
+	  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+	    mybutton.style.display = "block";
+	  } else {
+	    mybutton.style.display = "none";
+	  }
+	}
+
+	// When the user clicks on the button, scroll to the top of the document
+	function topFunction() {
+	  document.body.scrollTop = 0; // For Safari
+	  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+	}
+</script>
